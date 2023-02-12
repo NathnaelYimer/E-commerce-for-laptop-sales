2a367686f358ea13f3171c15665ea6ab5dedbc5f
dagiTensay <118015472+dagiTensay@users.noreply.github.com>
2023-02-13 01:59:21 +0300

    Update DynamicIndex.js

--- API/DynamicIndex.js
+++ API/DynamicIndex.js
@@ -1,9 +1,6 @@
 fetch('http://localhost:3000/store/products/getallproducts').then((data)=>{
     return data.json();
 }).then((completedata)=>{
-    // document.getElementById('root').
-    // innerHTML=completedata
-    // let data1="";
 completedata =completedata.slice(0,16);
 completedata.map((values)=>{
     document.getElementById("laptops").innerHTML+=`<div class="card rounded-4 m-5 p-0 col-sm-12 d-flex   "style="width: 15rem;height: 38rem;" >
@@ -39,40 +36,3 @@ completedata.map((values)=>{
 }).catch((err)=>{
     console.log(err);
 });
-
-
-
-
-
-
-
-// fetch('').then((data)=>{
-//     return data.json();
-// }).then((completedata)=>{
-//     // document.getElementById('root').
-//     // innerHTML=completedata
-//     let data1="";
-//     number = 1
-// // completedata =completedata.slice(17,22);
-// completedata.map((values)=>{
-//         number += 1
-//         data1 += `<tr>
-//             <td class="pt-2">${number}</td>
-//             <td class="pt-2">
-//               <img
-//                 src=${values.image_url}
-//                 style="width: 70px"
-//               />
-//             </td>
-//             <td>${values.name}</td>
-//             <td class="pt-2 pl-4">
-//               <a href="#"><i class="far fa-times-circle"></i></a>
-//             </td>
-//             <td class="pt-2">${values.price}</td>
-//           </tr>`
-//     });
-
-//     document.getElementById("laptops").innerHTML=data1;
-// }).catch((err)=>{
-//     console.log(err);
-// });
