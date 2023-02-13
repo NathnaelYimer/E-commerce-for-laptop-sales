1cee4a391637b904e7cd9ee6efa5e4f338300e9f
dagiTensay <118015472+dagiTensay@users.noreply.github.com>
2023-02-13 03:19:42 +0300

    Update adminAdd.js

--- API/adminAdd.js
+++ API/adminAdd.js
@@ -1,15 +1,12 @@
-async function nfunction() {
-    event.preventDefault();
-
-
+async function addfunction() {
     const name = document.getElementById('name').value
-    const price = document.getElementById('Price').value
-    const image = document.getElementById('Image').value
+    const price = document.getElementById('price').value
+    const image_url = document.getElementById('img-url').value
     const description = document.getElementById('description').value
     
     const response = await fetch("http://localhost:3000/store/products/addProduct", {
     method: "POST",
-    body: JSON.stringify({name,price,image,description }),
+    body: JSON.stringify({name,price,image_url,description}),
     redirect: 'manual',
     headers: {
       "Content-Type": "application/json",
@@ -19,5 +16,8 @@ async function nfunction() {
     if (response.ok) {
         console.log("Added succesfully!");
         window.location.href = 'updateproductdetails.html';
+    }
+    else {
+        alert("not correct")
     }
 }
